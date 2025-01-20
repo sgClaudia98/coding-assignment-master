fe85eedc2b553f9491184c612714e265bee5d9cd
Claudia SG <claudia.sg2611@gmail.com>
2025-01-20 13:21:59 +0100

    Feat: infinite scroll on movie list

--- src/hooks/useFetchMovies.jsx
+++ src/hooks/useFetchMovies.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchMovies } from "../data/moviesSlice";
 import useInfiniteScroll from "./useInfiniteScroll";
-import { ENDPOINT_SEARCH, ENDPOINT_DISCOVER, API_KEY } from "../constants";
+import { ENDPOINT_SEARCH, ENDPOINT_DISCOVER } from "../constants";
 
 const useFetchMovies = (searchQuery) => {
   const dispatch = useDispatch();
@@ -12,28 +12,25 @@ const useFetchMovies = (searchQuery) => {
 
   useEffect(() => {
     setMovies([]); // reset movies on search change
-    console.debug(page, "set", 1);
     setPage(1);
   }, [searchQuery]);
 
   useEffect(() => {
-    console.debug(page);
     loadMovies();
-  }, [page]);
+  }, [page, loadMovies]);
 
-  const loadMovies = () => {
+  const loadMovies = useCallback(() => {
     const endpoint = searchQuery
       ? `${ENDPOINT_SEARCH}&query=${searchQuery}&page=${page}`
       : `${ENDPOINT_DISCOVER}&page=${page}`;
     dispatch(fetchMovies(endpoint));
-  };
+  }, [dispatch, searchQuery, page]);
 
   useEffect(() => {
-    if (movies.fetchStatus == "success") {
-      if (movies.movies.results) {
+    if (movies.fetchStatus == "success" && movies.movies.results) {
         setMovies((prevMovies) => [...prevMovies, ...movies.movies.results]);
       }
-    }
+    
     setLoading(movies.fetchStatus != "loading");
   }, [movies.fetchStatus]);
 
